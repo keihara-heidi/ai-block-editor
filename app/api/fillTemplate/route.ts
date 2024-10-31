2e3727f3c027e4a2e71ae5c3a21f0662ff315c06
Keisuke Hara <keisuke@heidihealth.com>
2024-10-31 13:47:25 +1100

    reducing

--- app/api/fillTemplate/route.ts
+++ app/api/fillTemplate/route.ts
@@ -10,7 +10,7 @@ export type FillTemplateRequest = {
   model: models;
 };
 export const dynamic = 'force-dynamic';
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export async function POST(request: NextRequest) {
   try {
