52be5ed1c9dc4500ad700f56371a0bc43ce7ee31
Keisuke Hara <keisuke@heidihealth.com>
2024-12-19 14:55:47 +0900

    allow html input

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,16 @@ export default function Home() {
       Paragraph,
       ListItem,
       Gapcursor,
-      Markdown,
+      Markdown.configure({
+        html: true, // Allow HTML input/output
+        tightLists: true, // No <p> inside <li> in markdown output
+        tightListClass: 'tight', // Add class to <ul> allowing you to remove <p> margins when tight
+        bulletListMarker: '-', // <li> prefix in markdown output
+        linkify: false, // Create links from "https://..." text
+        breaks: false, // New lines (\n) in markdown input are converted to <br>
+        transformPastedText: true, // Allow to paste markdown text in the editor
+        transformCopiedText: false, // Copied text is transformed to markdown
+      }),
       Heading.configure({
         HTMLAttributes: {
           class: 'text-slate-200',
